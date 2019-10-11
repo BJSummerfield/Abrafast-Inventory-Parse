9a4ab58cd5328c1347d3ccdc9b92e4c8273fcbab
Brian Summerfield <brianjsummerfield@gmail.com>
2019-10-11 09:47:34 -0500

    edit condencer

--- condencer.rb
+++ condencer.rb
@@ -1,23 +1,29 @@
 require 'csv'
-edit = CSV.read('../csv/sa.csv')
+edit = CSV.read('../csv/a490canada.csv')
 #name = 1 Variation = 50
 #Select Diameter| 5/16" Phillips Head; 3/8" Hex Head; 1/2" Hex Head; 11/16" Hex Head; ~Grip Length| .08" - .40"; .031" - .63";
 #Thin Wall (TW) Bolt Zinc Nickle| Select Diameter| 5/16" Phillips Head;Grip Length| .08" - .40"
 i = 0
-finishes = []
+finishes = ['Plain']
 diameters =[]
 lengths = []
+variation = ""
 edit.each do |item|
   if i >= 1
     if item[1].include?('Length')
-      finish = 'Plain'
+      finish = 'Plain' if item[1].include?('Plain')
       finish = 'Hot Dipped Galvanized' if item[1].include?('Galvanized')
-      diameter = item[1].split(' ')[-4]
+      item[1] = item[1].gsub('  ', ' ')
+      item[1] = item[1].gsub('- 9','-9')
+      item[1] = item[1].gsub('- 8','-8')
+      item[1] = item[1].gsub('- 7','-7')
+
+      diameter = item[1].split(' ')[4]
       length = item[1].split(' ').pop
       diameters << diameter unless diameters.include?(diameter)
       finishes << finish unless finishes.include?(finish)
       lengths << length unless lengths.include?(length)
-      item[1] = "A325 Assembled Heavy Hex Bolt, Nut & Washer| Finish| #{finish};Diameter| #{diameter};Length| #{length}"
+      item[1] = "A490 Heavy Hex Bolt - Canadian| Finish| #{finish};Diameter| #{diameter};Length| #{length}"
     else
       item[52] = "True"
     end
@@ -25,16 +31,15 @@ edit.each do |item|
   i += 1
 end
 
-variation = ""
-diameters = diameters.sort_by { |s| s.split('-').map(&:to_r).inject(:+)}
+diameters = diameters.sort_by { |s| s.split('"')[0].split('-').map(&:to_r).inject(:+)}
 lengths = lengths.sort_by { |s| s.split('-').map(&:to_r).inject(:+)}
 
 variation += "Finish |"
 finishes.each do |i|
   variation += " #{i};"
 end
-# p diameters
-variation += "~Diameter|"
+
+variation += " ~Diameter|"
 diameters.each do |i|
   variation += " #{i};"
 end
@@ -47,7 +52,7 @@ edit[1][0] = nil
 edit[1][4] = nil
 edit[1][52] = 'False'
 edit[1][50] = variation
-edit[1][1] = "A325 Assembled Heavy Hex Bolt, Nut & Washer"
+edit[1][1] = "A490 Heavy Hex Bolt - Canadian"
 edit[1][25] = "Structural Products ~ Structural Bolts"
 
 CSV.open("../csv/FIX.CSV", "wb") do |csv|
